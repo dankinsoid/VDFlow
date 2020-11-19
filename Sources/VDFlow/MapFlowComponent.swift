74e8f8db1e381bc4db7f040a0a2dc991921cffe0
Danil <>
2020-11-19 22:32:54 +0300

    no message

--- Sources/VDFlow/MapFlowComponent.swift
+++ Sources/VDFlow/MapFlowComponent.swift
@@ -7,15 +7,19 @@
 
 import Foundation
 
-public struct MapFlowComponent<Base: FlowComponent, To>: WrapperComponentProtocol {
+public struct MapFlowComponent<Base: FlowComponent, Value>: FlowComponent, WrapperAnyComponentProtocol {
+	public static var baseType: AnyFlowComponent.Type { Base.self }
+	public var baseAny: AnyFlowComponent { base }
 	public let base: Base
-	let map: (To) -> Base.Value
+	public var id: String { base.id }
+	public var contentType: Any.Type { base.contentType }
+	let map: (Value) -> Base.Value
 	
 	public func create() -> Base.Content {
 		base.create()
 	}
 	
-	public func update(content: Base.Content, data: To?) {
+	public func update(content: Base.Content, data: Value?) {
 		base.update(content: content, data: data.map(map))
 	}
 	
