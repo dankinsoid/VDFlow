610f8519e82462e051cd4737686cc78d348b9df0
Danil <voidilov@gmail.com>
2021-02-08 12:45:22 +0300

    1.0.35

---
+++ Sources/VDFlow/VC.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  
+//
+//  Created by Данил Войдилов on 08.02.2021.
+//
+
+import Foundation
+
+public struct VC<Content>: FlowComponent {
+	
+	private let createClosure: () -> Content
+	
+	public init(_ create: @escaping () -> Content) {
+		createClosure = create
+	}
+	
+	public func create() -> Content {
+		createClosure()
+	}
+	
+}
--- VDFlow.podspec
+++ VDFlow.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'VDFlow'
-  s.version          = '1.0.34'
+  s.version          = '1.0.35'
   s.summary          = 'A short description of VDAnimation.'
   s.description      = <<-DESC
 TODO: Add long description of the pod here.
