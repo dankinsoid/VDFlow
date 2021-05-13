988a6f6b53df90dac655c8ff1b87c95d579b2d50
Danil <voidilov@gmail.com>
2021-05-13 17:06:47 +0300

    1.32.0

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
 		.library(name: "VDFlow", targets: ["VDFlow"]),
 	],
 	dependencies: [
-		.package(url: "https://github.com/dankinsoid/VDKit.git", from: "1.41.0"),
+		.package(url: "https://github.com/dankinsoid/VDKit.git", from: "1.45.0"),
 	],
 	targets: [
 		.target(name: "VDFlow", dependencies: ["VDKit"])
