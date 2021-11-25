7cba242649724d2c9296de49bb92762763f6c8d4
dankinsoid <voidilov@gmail.com>
2021-11-25 18:47:22 +0300

    2.38.0

--- Sources/VDFlow/NavigationStepDestionation.swift
+++ Sources/VDFlow/NavigationStepDestionation.swift
@@ -10,7 +10,7 @@ import SwiftUI
 extension NavigationLink {
 	
 	public init<Dest: View, T, D>(step: StateStep<T>.StepBinding<D>, @ViewBuilder destination: () -> Dest, @ViewBuilder label: () -> Label) where Destination == NavigationStepDestionation<Dest, D> {
-		self.init(tag: step.rootBinding.wrappedValue.key(step.keyPath), selection: step.rootBinding.selected.optional) {
+		self.init(isActive: step.rootBinding[isSelected: step.keyPath]) {
 			NavigationStepDestionation(content: destination(), stepBinding: step.binding)
 		} label: {
 			label()
